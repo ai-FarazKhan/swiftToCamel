66227d0588a31ef4fba4a38c24f7edd8de512d73
ai-FarazKhan <ai.farazalikhan@gmail.com>
2022-08-24 00:10:50 +0500

    .

---
+++ src/main/java/com/faraz/camel/swiftToCamel/JsonData.java
@@ -0,0 +1,23 @@
+package com.faraz.camel.swiftToCamel;
+
+public class JsonData {
+
+    private String sender;
+    private String messageType;
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getMessageType() {
+        return messageType;
+    }
+
+    public void setMessageType(String messageType) {
+        this.messageType = messageType;
+    }
+}
--- src/main/java/com/faraz/camel/swiftToCamel/RouterNew.java
+++ src/main/java/com/faraz/camel/swiftToCamel/RouterNew.java
@@ -1,44 +1,69 @@
 package com.faraz.camel.swiftToCamel;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.prowidesoftware.swift.model.mt.AbstractMT;
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Component
 public class RouterNew extends RouteBuilder {
+    JsonData jsonData = new JsonData();
+
     @Override
     public void configure() throws Exception {
-
-//        restConfiguration().component("servlet").host("0.0.0.0").port(8081).bindingMode(RestBindingMode.json).enableCORS(true);
-//        rest().path("/helloWorld").get().route()
-//        rest("/someRoute")
-//                .id("someRoute")
-//                .description("Some description")
-//                .post("/testing");
-
 
         from("file:C:/Users/Dell/Desktop/Folder One")
                 .process(new Processor() {
                     @Override
                     public void process(Exchange exchange) throws Exception {
+
                         Message input = exchange.getIn();
                         // reading body as a string from input message
                         String data = input.getBody(String.class);
-
                         AbstractMT msg = AbstractMT.parse(data);
-                        String modifiedData = "{sender: "+msg.getSender()+", type: "+msg.getMessageType()+"}";
+                        String modifiedData = "{sender: " + msg.getSender() + ", type: " + msg.getMessageType() + "}";
+                        jsonData.setSender(msg.getSender());
+                        jsonData.setMessageType(msg.getMessageType());
+
+//                        System.out.println(jsonData.getSender());
 
                         Message output = exchange.getMessage();
                         output.setBody(modifiedData);
+
+                        Map map = new HashMap();
+                        map.put("sender", jsonData.getSender());
+                        map.put("message type", jsonData.getMessageType());
+
+
+                        Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+                        String jsonString = gson.toJson(map);
+
+                        System.out.println(jsonString);
+
+                        restConfiguration().component("jetty").host("0.0.0.0").port(8081).bindingMode(RestBindingMode.json).enableCORS(true);
+
+                        rest("/swiftTo")
+                                .post("/camel").to("direct:camel");
+
+                        from("direct:camel")
+                                .transform().constant(jsonString);
+
 
                     }
 
                 })
                 .to("file:C:/Users/Dell/Desktop/Folder Two?fileName=MT103.json");
+
+
 
     }
 
